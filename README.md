4090d5e35d9d76c7e2ff3914745b7ed2d94fb34a
Sayantan Roy <sayantan.roy94@gmail.com>
2021-08-24 03:29:50 +0530

    updated apis for commit & contribs

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 [![Node JS](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)](https://nodejs.org/)
 [![Express JS](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white)](https://expressjs.com/)
+[![Vercel](https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white)](https://vercel.com/)
 
 [![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg?style=for-the-badge)](http://perso.crans.org/besson/LICENSE.html)
 [![Open Source? Yes!](https://img.shields.io/badge/Open_Source%3F-Yes!-blue?style=for-the-badge&logo=gitHub&logoColor=white)](https://opensource.com/resources/what-open-source/)
 ![Language](https://img.shields.io/github/languages/top/dusk196/github-total-commit?style=for-the-badge)
 ![Size](https://img.shields.io/github/languages/code-size/dusk196/github-total-commit?style=for-the-badge)
+
+![Contributions](https://github-total-commit.vercel.app/github/dusk196/how-many-days?style=for-the-badge&color=green)
 
 ### A simple badge for GitHub Repositories that shows total number of contributions since it's inception!
--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,14 @@
 var express = require('express');
-var axios = require('axios');
 var router = express.Router();
+var githubRouter = require('./github');
 
-/* GET landing page. */
+/**
+  * GET landing page.
+ **/
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Github Total Commit' });
 });
 
-/**
-  * GET API value.
-  * /github/{dusk196}/{github-total-commit}?style={for-the-badge}&color={green}
- **/
-router.get('/github/:user/:repo', function (req, res, next) {
-
-  var fetchInfo = 'https://api.github.com/repos/' + req.params.user + '/' + req.params.repo + '/contributors';
-  var info;
-
-  axios.get(fetchInfo)
-    .then(function (response) {
-      info = response.data[0].contributions;
-    })
-    .catch(function (error) {
-      console.error(error);
-      info = 'unable_to_fetch_details';
-    })
-    .then(function () {
-
-      var fetchSvg = 'https://img.shields.io/badge/total_commits-' + info + '-' + req.query.color + '?style=' + req.query.style + '&logo=github';
-      var finalResponse;
-
-      axios.get(fetchSvg)
-        .then(function (response) {
-          finalResponse = response.data;
-        })
-        .catch(function (error) {
-          console.error(error);
-        })
-        .then(function () {
-          return res.header('Content-Type', 'image/svg+xml;charset=utf-8').send(finalResponse);
-        });
-
-    });
-
-});
+router.use(githubRouter);
 
 module.exports = router;
